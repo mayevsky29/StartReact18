de1c6d527f72f961d18dc3f4a3d52fe9bd042f5a
mayevsky29 <mayevsky29@ukr.net>
2022-05-10 21:58:39 +0300

    react 18 and router 6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
+import Batching from "./newfitchs/batching";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // Налаштування маршрутів; навігація по сайту
+  // BrowserRouter підключення до URL адреси
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="batching" element={<Batching />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />}>
+      {/* індексний маршрут, дочірній маршрут по замовчуванню для батьківського,
+      відображається на шляху до батькііського маршруту,
+       поки не зроблений вибір*/}
+      <Route
+        index
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>Виберіть рахунок</p>
+          </main>
+        }
+      />
+         {/* Invoice дочірній елемент від Invoices */}
+        <Route path=":invoiceId" element={<Invoice />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>"Тут нічого не має!"</p>
+          </main>
+        }
+      ></Route>
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/newfitchs/batching.jsx
@@ -0,0 +1,22 @@
+import { useState, fetchSomething } from "react";
+
+export default function Batching() {
+  const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(false);
+
+  function handleClick() {
+    fetchSomething().then(() => {
+      // React 17 and earlier does NOT batch these because
+      // they run *after* the event in a callback, not *during* it
+      setCount(c => c + 1); // Causes a re-render
+      setFlag(f => !f); // Causes a re-render
+    });
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Next</button>
+      <h1 style={{ color: flag ? "blue" : "black" }}>{count}</h1>
+    </div>
+  );
+}
